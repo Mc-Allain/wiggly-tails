b07d1825a48925a580d2b2c3f62d78b501d3f7fe
Mc-Allain <princemcallain@gmail.com>
2020-12-03 01:55:46 +0800

    Modified

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'jquery/dist/jquery.js'
 
 ReactDOM.render(
   <React.StrictMode>
